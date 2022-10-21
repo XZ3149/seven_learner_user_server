abbd29f364de20711c5bbd305a756ab4f8873a55
XZ3149 <114506836+XZ3149@users.noreply.github.com>
2022-10-21 13:07:11 -0400

    Add files via upload

---
+++ src/UserResource.py
@@ -0,0 +1,231 @@
+import pymysql
+import os
+import random
+
+class UserResource:
+
+    def __int__(self):
+        pass
+
+    @staticmethod
+    def _get_connection():
+        usr = os.environ.get("DBUSER")
+        pw = os.environ.get("DBPW")
+        h = os.environ.get("DBHOST")
+
+        conn = pymysql.connect(
+            user= usr,
+            password= pw,
+            host= h,
+            cursorclass=pymysql.cursors.DictCursor,
+            autocommit=True
+        )
+        return conn
+
+
+
+    @staticmethod
+    def get_by_AccountID(key):
+
+        sql = "SELECT AccountID, FirstName, LastName, Email FROM Userinfor.user where AccountID=%s";
+        conn = UserResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=key)
+        result = cur.fetchone()
+        return result
+
+    def get_users(limit = 10, offset = 0):
+        sql = f'SELECT AccountID, FirstName, LastName, Email FROM Userinfor.user limit {limit} offset {offset}';
+        conn = UserResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql)
+        result = cur.fetchall()
+        return result
+
+    def get_user_by_query(FirstName, LastName, Email, limit = 20, offset = 0):
+
+        query_str = 'where'
+        if FirstName:
+            query_str += f' FirstName="{FirstName}" and'
+        if LastName:
+            query_str += f' LastName="{LastName}" and'
+        if Email:
+            query_str += f' Email="{Email}" and'
+        query_str = query_str[:-4]
+
+        conn = UserResource._get_connection()
+        cur = conn.cursor()
+        sql = f'SELECT AccountID, FirstName, LastName, Email FROM Userinfor.user {query_str} ORDER BY LastName limit ' \
+              f'{limit} offset {offset}';
+        print(sql)
+        res = cur.execute(sql)
+        result = cur.fetchall()
+        return result
+
+    def check_avalibility(Email):
+        sql = f'SELECT count(*) FROM Userinfor.user where Email="{Email}"';
+        conn = UserResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql)
+        result = cur.fetchone()
+        if result['count(*)'] == 0:
+            return True
+        else:
+            return False
+
+
+
+    def count_rows(table_name, conditions = ''):
+        sql = f'SELECT count(*) FROM Userinfor.{table_name} {conditions}';
+        conn = UserResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql)
+        result = cur.fetchone()
+
+        return result
+
+
+    def create_accountID(FirstName, LastName, MiddleName, Email, Password):
+        """this is the function use to create account"""
+        AccountID = random.randrange(10000000,99999999)
+        conn = UserResource._get_connection()
+        cur = conn.cursor()
+        sql = 'SELECT * FROM Userinfor.user where AccountID=%s'
+
+        while (cur.execute(sql, AccountID)):
+            AccountID = random.randrange(10000000, 99999999)
+
+        sql = 'insert into Userinfor.user (AccountID, FirstName, LastName,MiddleName,Email,Password) values (%s, %s, %s,%s, %s,%s)'
+        res = cur.execute(sql, [AccountID,FirstName, LastName, MiddleName, Email, Password])
+        if res:
+            return AccountID
+        return res
+
+    def update_user_infor(AccountID, FirstName, LastName, MiddleName, Password):
+        sql = 'update Userinfor.user set '
+
+        if FirstName:
+            sql += f'FirstName = %s, '
+        if LastName:
+            sql += f'LastName = %s, '
+        if MiddleName:
+            sql += f'MiddleName = %s, '
+        if Password:
+            sql += f'Password = %s, '
+
+        sql = sql[:-2] + f' where AccountID = {AccountID}'
+        print(sql)
+        values = []
+        for i in [FirstName,LastName,MiddleName,Password]:
+            if i:
+                values += [[i]]
+        print(values)
+        conn = UserResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, values)
+        return res
+
+
+    def delete_account(AccountID):
+        """this is the function use to delete an account"""
+
+        sql_order = f'delete from Userinfor.UserOrder where UserAccountID=%s'
+        sql_restaurants = f'delete from Userinfor.FavoriteRestaurants where UserAccountID=%s'
+        sql_user= f'delete from Userinfor.user where AccountID=%s'
+        conn = UserResource._get_connection()
+        cur = conn.cursor()
+        cur.execute(sql_order, AccountID)
+        cur.execute(sql_restaurants,AccountID)
+        result = cur.execute(sql_user, AccountID)
+
+        return result
+
+    def get_order_by_userID(AccountID, limit = 20, offset = 0):
+        """this is the function use to get order information for a user"""
+        sql = f'select OrderID from Userinfor.UserOrder where UserAccountID=%s limit {limit} offset {offset}'
+        conn = UserResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, AccountID)
+        result = cur.fetchall()
+
+        return result
+
+    def check_duplicate_orderID(orderID):
+        sql = f'select * from Userinfor.UserOrder where OrderID=%s'
+        conn = UserResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, orderID)
+        result = cur.fetchall()
+        if result:
+            return True
+        else:
+            return False
+
+
+    def add_user_order(AccountID, OrderID):
+        sql = f'insert into Userinfor.UserOrder values ({AccountID}, {OrderID})'
+        conn = UserResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql)
+
+        return res
+
+    def delete_user_order(OrderID):
+        sql = f'delete from Userinfor.UserOrder where OrderID= {OrderID}'
+        conn = UserResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql)
+        return res
+
+
+    def get_restaurants_by_userID(AccountID, limit = 20, offset = 0):
+        """this is the function use to get order information for a user"""
+        sql = f'select RestaurantID, RestaurantName from Userinfor.FavoriteRestaurants where UserAccountID=%s limit {limit} offset {offset}'
+        conn = UserResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, AccountID)
+        result = cur.fetchall()
+
+        return result
+
+    def check_duplicate_RestaurantID(RestaurantID):
+        sql = f'select * from Userinfor.FavoriteRestaurants where RestaurantID=%s'
+        conn = UserResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, RestaurantID)
+        result = cur.fetchall()
+        if result:
+            return True
+        else:
+            return False
+
+
+    def add_user_restaurant(AccountID, RestaurantID, RestaurantName):
+        sql = f'insert into Userinfor.FavoriteRestaurants values ({AccountID}, {RestaurantID}, "{RestaurantName}")'
+        conn = UserResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql)
+        return res
+
+    def delete_user_restaurant(RestaurantID):
+        sql = f'delete from Userinfor.FavoriteRestaurants where RestaurantID= {RestaurantID}'
+        conn = UserResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql)
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/application.py
@@ -0,0 +1,251 @@
+import re
+
+from flask import Flask, Response, request, redirect, url_for, request
+from datetime import datetime
+import json
+from UserResource import UserResource
+from flask_cors import CORS
+
+# Create the Flask application object.
+app = Flask(__name__)
+
+CORS(app)
+
+
+@app.get("/status/health")
+def get_status():
+    t = str(datetime.now())
+    msg = {
+        "name": "UserResource-Microservice",
+        "health": "Good",
+        "at time": t
+    }
+
+    result = Response(json.dumps(msg), status=200, content_type="application/json")
+    return result
+
+
+@app.route("/user/<AccountID>", methods=["GET", 'PUT', 'DELETE'])
+def get_user_by_account_id(AccountID):
+
+    if request.method == 'GET':
+        result = UserResource.get_by_AccountID(AccountID)
+        if result:
+            rsp = Response(json.dumps(result), status=200, content_type="application.json")
+        else:
+            rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+        return rsp
+
+    if request.method == "PUT":
+        FirstName = request.form.get('FirstName', default = None)
+        LastName = request.form.get('LastName', default = None)
+        Password = request.form.get('Password', default = None)
+        MiddleName = request.form.get('MiddleName', default = None)
+
+        if not FirstName and not LastName and not Password and not MiddleName:
+            rsp = Response("You need to update something", status=400, content_type="text/plain")
+        else:
+            UserResource.update_user_infor(AccountID,FirstName,LastName,MiddleName, Password)
+            rsp = redirect(url_for('get_user_by_account_id', AccountID=AccountID))
+
+        return rsp
+
+
+    if request.method == 'DELETE':
+        result = UserResource.delete_account(AccountID)
+
+        if result:
+            rsp = Response("Delete successful", status=200, content_type="text/plain")
+        else:
+            rsp = Response("Delete Fail, please check entry again", status=400, content_type="text/plain")
+    return rsp
+
+
+
+@app.route("/user/<AccountID>/UserOrders", methods=["GET", "POST", 'DELETE'])
+def get_user_order_by_accountID(AccountID):
+    # check whether user exist before checking all other things
+    head_infor = UserResource.get_by_AccountID(AccountID)
+    if not head_infor:
+        rsp = Response("USER NOT FOUND", status=404, content_type="text/plain")
+        return rsp
+    if request.method == 'GET':
+
+        per_page = request.args.get('per_page', default = 10, type = int) # define how many results you want per page
+        page = request.args.get('page', default = 1, type=int)
+        offset = (page - 1) * per_page  # offset for SQL query
+        count = len(UserResource.get_order_by_userID(AccountID, 10000, 0))
+        number_pages = (count // per_page) + 1
+
+        result = UserResource.get_order_by_userID(AccountID, per_page,offset)
+
+        if result:
+            result.insert(0, head_infor)
+            result[0]['page'] = page
+            result[0]['numberPages'] = number_pages
+            rsp = Response(json.dumps(result), status=200, content_type="application.json")
+        else:
+            rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+        return rsp
+
+    if request.method == "POST":
+        OrderID = request.form.get('OrderID', default = None, type = int)
+        if not OrderID:
+            rsp = Response("You need to enter a OrderID", status=400, content_type="text/plain")
+
+        elif UserResource.check_duplicate_orderID(OrderID):
+            rsp = Response("Duplicate OrderID", status=400, content_type="text/plain")
+
+        else:
+            res = UserResource.add_user_order(AccountID,OrderID)
+            if res:
+                rsp = redirect(url_for('get_user_order_by_accountID', AccountID=AccountID))
+            else:
+                rsp = Response("Update fail", status=400, content_type="text/plain")
+        return rsp
+
+
+    if request.method == 'DELETE':
+        OrderID = request.form.get('OrderID', default=None, type=int)
+        if not OrderID:
+            rsp = Response("You need to enter a OrderID", status=400, content_type="text/plain")
+
+        elif not UserResource.check_duplicate_orderID(OrderID):
+            rsp = Response("OrderID not exist", status=400, content_type="text/plain")
+        else:
+            res = UserResource.delete_user_order(OrderID)
+            if res:
+                rsp = rsp = redirect(url_for('get_user_order_by_accountID', AccountID=AccountID))
+            else:
+                rsp = Response("delete fail", status=400, content_type="text/plain")
+
+        return rsp
+
+
+@app.route("/user/<AccountID>/FavoriteRestaurants", methods=["GET", "POST", "DELETE"])
+def get_user_restaurants(AccountID):
+    head_infor = UserResource.get_by_AccountID(AccountID)
+    if not head_infor:
+        rsp = Response("USER NOT FOUND", status=404, content_type="text/plain")
+        return rsp
+    if request.method == 'GET':
+
+        per_page = request.args.get('per_page', default = 10, type = int) # define how many results you want per page
+        page = request.args.get('page', default = 1, type=int)
+        offset = (page - 1) * per_page  # offset for SQL query
+        count = len(UserResource.get_order_by_userID(AccountID, 10000, 0))
+        number_pages = (count // per_page) + 1
+
+        result = UserResource.get_restaurants_by_userID(AccountID, per_page,offset)
+
+        if result:
+            result.insert(0, head_infor)
+            result[0]['page'] = page
+            result[0]['numberPages'] = number_pages
+            rsp = Response(json.dumps(result), status=200, content_type="application.json")
+        else:
+            rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+        return rsp
+
+    if request.method == "POST":
+        RestaurantID = request.form.get('RestaurantID', default = None, type = int)
+        RestaurantName = request.form.get('RestaurantName', default='Unknown_restaurant', type=str)
+
+        if not RestaurantID:
+            rsp = Response("You need to enter a RestaurantID", status=400, content_type="text/plain")
+
+        elif UserResource.check_duplicate_RestaurantID(RestaurantID):
+            rsp = Response("Duplicate RestaurantID, you already favor this restaurant", status=400, content_type="text/plain")
+
+        else:
+            res = UserResource.add_user_restaurant(AccountID,RestaurantID,RestaurantName)
+            if res:
+                rsp = redirect(url_for('get_user_restaurants', AccountID=AccountID))
+            else:
+                rsp = Response("Update fail", status=400, content_type="text/plain")
+        return rsp
+
+    if request.method == 'DELETE':
+        RestaurantID = request.form.get('RestaurantID', default = None, type = int)
+        if not RestaurantID:
+            rsp = Response("You need to enter a RestaurantID", status=400, content_type="text/plain")
+        elif not UserResource.check_duplicate_RestaurantID(RestaurantID):
+            rsp = Response("Favorite restaurant not exist", status=400, content_type="text/plain")
+        else:
+            res = UserResource.delete_user_restaurant(RestaurantID)
+            if res:
+                rsp = rsp = redirect(url_for('get_user_restaurants', AccountID=AccountID))
+            else:
+                rsp = Response("delete fail", status=400, content_type="text/plain")
+        return rsp
+
+
+
+
+
+
+
+
+
+
+
+@app.route("/user", methods=["GET",'POST'])
+def get_user_infor():
+    if request.method == 'GET':
+        FirstName = request.args.get('FirstName', default = None)
+        LastName = request.args.get('LastName', default = None)
+        Email = request.args.get('Email', default = None)
+        per_page = request.args.get('per_page', default = 10, type = int) # define how many results you want per page
+        page = request.args.get('page', default = 1, type=int)
+        offset = (page - 1) * per_page  # offset for SQL query
+        #limit = request.args.get('limit', default = 50, type=int)
+
+        if FirstName or LastName or Email:
+            result = UserResource.get_user_by_query(FirstName, LastName, Email, per_page, offset)
+            count = len(UserResource.get_user_by_query(FirstName, LastName, Email, 1000, 0))
+        else:
+            count = UserResource.count_rows('user')['count(*)']
+            result = UserResource.get_users(per_page, offset)
+        if result:
+            number_pages = (count // per_page) + 1
+            result.insert(0, {'page': page, 'numberPages': number_pages})
+            rsp = Response(json.dumps(result), status=200, content_type="application.json")
+        else:
+            rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+        return rsp
+
+
+    if request.method == 'POST':
+        FirstName = request.form.get('FirstName', default = None)
+        LastName = request.form.get('LastName', default = None)
+        Email = request.form.get('Email', default = None)
+        Password = request.form.get('Password', default = None)
+        MiddleName = request.form.get('MiddleName', default = '')
+        if not FirstName or not LastName or not Password or not Email:
+            rsp = Response("Please Enter Correct Information", status=400, content_type="text/plain")
+
+        elif not UserResource.check_avalibility(Email):
+            rsp = Response("Email have been used", status=400, content_type="text/plain")
+        else:
+
+            result = UserResource.create_accountID(FirstName, LastName, MiddleName, Email, Password)
+            # if insertion happen, server will return accountID, else return 0
+            if result:
+                rsp = redirect(url_for('get_user_by_account_id', AccountID= result))
+            else:
+                rsp = Response("Fail to create Account", status=400, content_type="text/plain")
+        return rsp
+
+
+@app.route("/test_post", methods=["POST"])
+def test_post_method():
+    if request.method == 'POST':
+
+        user = request.form.get("id")
+        return user
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5011)
+
